44c109e804edbd30b1dc1b0c17845e50f3232e24
unknown <abie@.clients.nebula2.washington.edu>
2009-07-08 12:03:03 -0700

    better comments, and (slightly) better priors

--- bednets.py
+++ bednets.py
@@ -15,6 +15,11 @@ def load_csv(fname):
     -------
     returns a list of dicts, one list item for each row of the csv
     (keyed by the first row)
+
+    Notes
+    -----
+    every value in the dict will be a string.  remember to convert
+    numbers to floats before doing any math with them.
     """
     import csv
     f = open(fname)
@@ -36,25 +41,29 @@ retention_data = load_csv('retention07072009.csv')
 c = 'Zambia'
 
 ### find some averages to use as priors
-d_all = [float(d['Program_Itns']) for d in administrative_distribution_data if d['Country'] == c]
-d_avg = mean(d_all)
-d_ste = std(d_all)
+nd_all = [float(d['Program_Itns']) for d in administrative_distribution_data \
+             if d['Country'] == c]
+nd_avg = mean(nd_all)
+nd_ste = std(nd_all)
 
-
-### set up all stochastic vars
 vars = []
+
+   #######################
+  ### compartmental model
+ ###
+#######################
 
 p_l = Beta('Pr[net is lost]', alpha=2., beta=10.)
 
 # TODO: consider choosing better priors
-nd = Lognormal('nets distributed', mu=log(d_avg) * ones(10), tau=1.)
-nm = Lognormal('nets manufactured', mu=log(d_avg) * ones(10), tau=1.)
+nd = Lognormal('nets distributed', mu=log(nd_avg) * ones(10), tau=1.)
+nm = Lognormal('nets manufactured', mu=log(nd_avg) * ones(10), tau=1.)
 
 # TODO: consider choosing better priors
-W_0 = Lognormal('initial warehouse stock', mu=log(1000), tau=1.)
-H_0 = Lognormal('initial household stock', mu=log(1000), tau=1.)
+W_0 = Lognormal('initial warehouse net stock', mu=log(1000), tau=1.)
+H_0 = Lognormal('initial household net stock', mu=log(1000), tau=1.)
 
-@deterministic
+@deterministic(name='warehouse net stock')
 def W(W_0=W_0, nm=nm, nd=nd):
     W = zeros(10)
     W[0] = W_0
@@ -62,7 +71,7 @@ def W(W_0=W_0, nm=nm, nd=nd):
         W[t+1] = W[t] + nm[t] - nd[t]
     return W
 
-@deterministic
+@deterministic(name='household net stock')
 def H(H_0=H_0, nd=nd, p_l=p_l):
     H = zeros(10)
     H[0] = H_0
@@ -72,24 +81,32 @@ def H(H_0=H_0, nd=nd, p_l=p_l):
 
 vars += [p_l, nd, nm, W_0, H_0, W, H]
 
-### stochastic and observations for net retention 
+
+   #####################
+  ### statistical model
+ ###
+#####################
+
+### observed net retention 
 # TODO: consider choosing better priors
-s_r = Gamma('standard error in retention rate', 10., 10./.0001)
+s_r = Gamma('standard error in retention rate', 10., 10./.01)
 
 retention_obs = []
 for d in retention_data:
     @observed
     @stochastic(name='retention_%s_%s' % (d['Name'], d['Year']))
-    def obs(value=float(d['Retention_Rate']), T_i=float(d['Follow_up_Time']), p_l=p_l, s_r=s_r):
+    def obs(value=float(d['Retention_Rate']),
+            T_i=float(d['Follow_up_Time']),
+            p_l=p_l, s_r=s_r):
         return normal_like(value, (1. - p_l) ** T_i, 1. / s_r**2)
     retention_obs.append(obs)
 
 vars += [s_r, retention_obs]
 
 
-### stochastic and observations for manufacturing flow
+### observed nets manufactured
 # TODO: consider choosing better priors
-s_m = Gamma('standard error in manufacturing data', 10., 10./1.e2)
+s_m = Gamma('standard error in manufacturing data', 10., 10./1.e4)
 
 manufacturing_obs = []
 for d in manufacturing_data:
@@ -105,13 +122,13 @@ for d in manufacturing_data:
 vars += [s_m, manufacturing_obs]
 
 
-### stochastic and observations for distribution flow
+### observed nets distributed
 # TODO: consider choosing better priors
-s_d = Gamma('standard error in distribution data', 10., 10./1.e2)
+s_d = Gamma('standard error in administrative distribution data', 10., 10./1.e4)
 
 admin_distribution_obs = []
 for d in administrative_distribution_data:
-    if d['Country'] !=c:
+    if d['Country'] != c:
         continue
 
     @observed
@@ -130,17 +147,21 @@ for d in household_distribution_data:
 
     @observed
     @stochastic(name='household_distribution_%s_%s' % (d['Name'], d['Year']))
-    def obs(value=float(d['Survey_Itns']), estimate_year=int(d['Year']), survey_year=int(d['Survey_Year']),
-            std_err=float(d['Ste_Survey_Itns']), nd=nd, p_l=p_l):
+    def obs(value=float(d['Survey_Itns']),
+            estimate_year=int(d['Year']),
+            survey_year=int(d['Survey_Year']),
+            std_err=float(d['Ste_Survey_Itns']),
+            nd=nd, p_l=p_l):
         return normal_like(value,
-                           nd[estimate_year - 2000] * (1 - p_l) ** (survey_year - estimate_year),
+                           nd[estimate_year - 2000] * \
+                               (1 - p_l) ** (survey_year - estimate_year),
                            1./ std_err**2)
     household_distribution_obs.append(obs)
 
 vars += [household_distribution_obs]
 
 
-### stochastic and observations for household net stock
+### observed household net stocks
 household_stock_obs = []
 for d in household_stock_data:
     if d['Name'] != c:
@@ -148,7 +169,9 @@ for d in household_stock_data:
 
     @observed
     @stochastic(name='household_stock_%s_%s' % (d['Name'], d['Year']))
-    def obs(value=float(d['Survey_Itns']), year=int(d['Year']), std_err=float(d['Ste_Survey_Itns']),
+    def obs(value=float(d['Survey_Itns']),
+            year=int(d['Year']),
+            std_err=float(d['Ste_Survey_Itns']),
             H=H):
         return normal_like(value, H[year-2000], 1./ std_err**2)
     household_stock_obs.append(obs)
@@ -156,6 +179,18 @@ for d in household_stock_data:
 vars += [household_stock_obs]
 
 
+   #####################
+  ### additional priors
+ ###
+#####################
+
+@potential
+def smooth_H(H=H):
+    return normal_like(H[:-1] / H[1:], 1., 1.)
+
+@potential
+def smooth_W(W=W):
+    return normal_like(W[:-1] / W[1:], 1., 1.)
 
 print 'running MCMC...'
 mc = MCMC(vars)
@@ -164,27 +199,28 @@ mc.sample(20000,10000,20)
 
 clf()
 
-def plot_fit(f):
-    plot(range(2000,2010), f.stats()['mean'], 'k', linewidth=2, alpha=.9)
-    plot(range(2000,2010), f.stats()['quantiles'][2.5], 'k:', linewidth=2, alpha=.5)
-    plot(range(2000,2010), f.stats()['quantiles'][97.5], 'k:', linewidth=2, alpha=.5)
+def plot_fit(f, scale=1.e6):
+    plot(range(2000,2010), f.stats()['mean']/scale, 'k', linewidth=2, alpha=.9)
+    plot(range(2000,2010), f.stats()['quantiles'][2.5]/scale, 'k:', linewidth=2, alpha=.5)
+    plot(range(2000,2010), f.stats()['quantiles'][97.5]/scale, 'k:', linewidth=2, alpha=.5)
 
-def scatter_data(data_list, country, country_key, data_key, error_key=None, error_val=None, fmt='gs'):
+def scatter_data(data_list, country, country_key, data_key, error_key=None, error_val=None, fmt='gs', scale=1.e6):
     if error_key:
-        error_val = [1.96*float(d[error_key]) for d in data_list if d[country_key] == c]
+        error_val = [1.96*float(d[error_key])/scale \
+                         for d in data_list if d[country_key] == c]
     errorbar([float(d['Year']) for d in data_list if d[country_key] == c],
-             [float(d[data_key]) for d in data_list if d[country_key] == c],
+             [float(d[data_key])/scale for d in data_list if d[country_key] == c],
              error_val, fmt=fmt, alpha=.5)
 
 
-subplot(2,2,1)
+subplot(2,3,1)
 title('nets manufactured')
 plot_fit(nm)
 scatter_data(manufacturing_data, c, 'Country', 'Manu_Itns',
              error_val=1.96 * s_m.stats()['mean'])
 
 
-subplot(2,2,2)
+subplot(2,3,2)
 title('nets distributed')
 plot_fit(nd)
 scatter_data(administrative_distribution_data, c, 'Country', 'Program_Itns',
@@ -193,14 +229,15 @@ scatter_data(household_distribution_data, c, 'Name', 'Survey_Itns',
              error_key='Ste_Survey_Itns', fmt='bs')
 
 
-subplot(2,2,3)
+subplot(2,3,3)
 title('nets in warehouse')
 plot_fit(W)
 
 
-subplot(2,2,4)
+subplot(2,3,4)
 title('nets in households')
 plot_fit(H)
 scatter_data(household_stock_data, c, 'Name', 'Survey_Itns',
              error_key='Ste_Survey_Itns', fmt='bs')
 
+subplot(2,3,5)
