683d425fcc6743893e6cc81980a3d4eb33c0b86d
Abraham Flaxman <abie@u.washington.edu>
2009-07-30 17:56:33 -0700

    better xticks

--- bednets.py
+++ bednets.py
@@ -399,7 +399,7 @@ for c in sorted(country_set):
 
         if str(stoch).find('distribution waiting time') == -1:
             a,l = xticks()
-            l = [int(floor(a*100.)/100.) for x in a]
+            l = [int(floor(a*100.)) for x in a]
             l[0] = str(l[0]) + '%'
             xticks(floor(a*100.)/100., l, fontsize=small_fontsize)
         title(str(stoch), fontsize=small_fontsize)
