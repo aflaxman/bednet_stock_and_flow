cfa84a44ede895503ab320d2cd17b0145a782373
abie <abie@alap.domain.actdsltmp>
2009-09-21 00:09:43 -0700

    add method to generate consistient model variables from mean of data

--- data.py
+++ data.py
@@ -4,7 +4,7 @@ for bednet distribution
 
 import csv
 import time
-from numpy import zeros
+from numpy import zeros, mean
 
 import settings
 
@@ -74,3 +74,52 @@ def population_for(c, year_start, year_end):
             pop_vec[ii] = pop_vec[ii-1]
 
     return pop_vec
+
+def add_synthetic_data():
+    # find population by country-year
+    pop = {}
+    for d in population:
+        pop[(d['Country'], d['Year'])] = d['Pop'] * 1000.
+
+    # find mean LLINs manu and dist per capita by year
+    manu = [[] for y in range(settings.year_start, settings.year_end)]
+    for d in llin_manu:
+        if pop.has_key((d['Country'], d['Year'])):
+            manu[int(d['Year']-settings.year_start)].append(
+                d['Manu_Itns'] / pop[(d['Country'], d['Year'])])
+
+    mu_manu = zeros(len(manu))
+    for ii in range(settings.year_end - settings.year_start):
+        if manu[ii]:
+            mu_manu[ii] = mean(manu[ii])
+    mu_manu += .005
+
+    dist = [[] for y in range(settings.year_start, settings.year_end)]
+    for d in hh_llin_flow:
+        if pop.has_key((d['Country'], d['Year'])):
+            dist[int(d['Year']-settings.year_start)].append(
+                d['Total_LLINs'] / pop[(d['Country'], d['Year'])])
+
+    mu_dist = zeros(len(dist))
+    for ii in range(settings.year_end - settings.year_start):
+        if dist[ii]:
+            mu_dist[ii] = mean(dist[ii])
+
+
+    mu = mu_manu * 1000000
+    delta = mu_dist * 1000000
+
+    Psi = zeros(len(mu))
+    Th1 = zeros(len(mu))
+    Th2 = zeros(len(mu))
+    Th3 = zeros(len(mu))
+    Th4 = zeros(len(mu))
+
+    for ii in range(0,len(mu)-1):
+        Psi[ii+1] = Psi[ii] + mu[ii] - delta[ii]
+        Th1[ii+1] = delta[ii]
+        Th2[ii+1] = Th1[ii]
+        Th3[ii+1] = Th2[ii]
+        Th4[ii+1] = Th3[ii]
+            
+    return mu, delta, Psi, Th1, Th2, Th3, Th4, Th1+Th2+Th3+Th4
